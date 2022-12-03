8634aed10295213282f7feb0afea0c38db8838f3
Mustafa Keser <keserzade@gmail.com>
2022-12-04 00:27:13 +0300

    v1.1

--- day03.sql
+++ day03.sql
@@ -267,6 +267,202 @@ vermek istedigimiz ismi yazariz */
   select count(maas) from calisanlar2  -- 7   - maas alanlarin sayisini verdi
   select count(*) from calisanlar2  -- 8   null satirinida saydi 8 verdi
   
+  /*
+Eğer count(*) kullanırsak tablodaki tüm satırların sayısını verir
+Sutun adı kullanırsak o sutundaki sayıları verir
+*/
+
+ select * from calisanlar2;
+  select * from markalar;
+  
+-- Her markanin id’sini, ismini ve toplam kaç şehirde bulunduğunu listeleyen bir 
+	--SORGU yaziniz.
+  
+  SELECT marka_id, marka_isim, 
+(SELECT count(sehir) FROM calisanlar2 where marka_isim=isyeri) as sehir_sayisi
+FROM markalar;
+  
+  
+ -- Her markanin ismini, calisan sayisini ve o markaya ait calisanlarin toplam maaşini listeleyiniz
+SELECT marka_isim, calisan_sayisi, 
+(SELECT sum(maas) FROM calisanlar2 WHERE marka_isim = isyeri) as toplam_maas 
+FROM markalar;
+  
+  -- Her markanin ismini, calisan sayisini ve o markaya ait calisanlarin
+  -- maksimum ve minumum maaşini listeleyen bir Sorgu yaziniz.
+SELECT marka_isim, calisan_sayisi, 
+(SELECT max(maas) FROM calisanlar2 WHERE marka_isim = isyeri) as max_maas,
+(SELECT min(maas) FROM calisanlar2 WHERE marka_isim = isyeri) as min_maas
+FROM markalar;
+  
+  --VIEW Kullanimi
+  
+  CREATE VIEW maxminmaas
+  AS 
+	SELECT marka_isim, calisan_sayisi, 
+	(SELECT max(maas) FROM calisanlar2 WHERE marka_isim = isyeri) as max_maas,
+	(SELECT min(maas) FROM calisanlar2 WHERE marka_isim = isyeri) as min_maas
+	FROM markalar;
+  
+  select * from maxminmaas;
+  
+  
+  
+  	/*					EXISTS CONDITION
+	EXISTS Condition subquery’ler ile kullanilir. IN ifadesinin kullanımına benzer
+olarak, EXISTS ve NOT EXISTS ifadeleri de alt sorgudan getirilen değerlerin içerisinde
+bir değerin olması veya olmaması durumunda işlem yapılmasını sağlar.*/
+  
+  
+  CREATE TABLE mart (
+urun_id int,
+musteri_isim varchar(50), 
+urun_isim varchar(50)
+);
+
+INSERT INTO mart VALUES (10, 'Mark', 'Honda');
+INSERT INTO mart VALUES (20, 'John', 'Toyota');
+INSERT INTO mart VALUES (30, 'Amy', 'Ford');
+INSERT INTO mart VALUES (20, 'Mark', 'Toyota');
+INSERT INTO mart VALUES (10, 'Adam', 'Honda');
+INSERT INTO mart VALUES (40, 'John', 'Hyundai');
+INSERT INTO mart VALUES (20, 'Eddie', 'Toyota');
+
+CREATE TABLE nisan (
+urun_id int ,
+musteri_isim varchar(50), 
+urun_isim varchar(50)
+);
+
+INSERT INTO nisan VALUES (10, 'Hasan', 'Honda');
+INSERT INTO nisan VALUES (10, 'Kemal', 'Honda');
+INSERT INTO nisan VALUES (20, 'Ayse', 'Toyota');
+INSERT INTO nisan VALUES (50, 'Yasar', 'Volvo');
+INSERT INTO nisan VALUES (20, 'Mine', 'Toyota');
+
+  select * from mart;
+  select * from nisan;
+  
+  
+  /*--MART VE NİSAN aylarında aynı URUN_ID ile satılan ürünlerin URUN_ID’lerini listeleyen 
+  ve aynı zamanda bu ürünleri MART ayında alan MUSTERI_ISIM 'lerini listeleyen bir sorgu yazınız.*/
+  
+  select urun_id, musteri_isim from mart where exists
+  (select urun_id from nisan where mart.urun_id = nisan.urun_id);
+  
+  
+ /* --Her iki ayda birden satılan ürünlerin URUN_ISIM'lerini ve bu ürünleri
+		NİSAN ayında satın alan MUSTERI_ISIM'lerini listeleyen bir sorgu yazınız.*/
+		
+	select urun_id, musteri_isim from nisan where exists
+  (select urun_id from mart where mart.urun_id = nisan.urun_id);
+
+  
+ /* --Her iki ayda ortak satilmayan ürünlerin URUN_ISIM'lerini ve bu ürünleri
+NİSAN ayında satın alan MUSTERI_ISIM'lerini listeleyen bir sorgu yazınız.*/
+
+	select urun_isim, musteri_isim from mart where not exists
+	(select urun_isim from nisan where mart.urun_id = nisan.urun_id);
+
+  
+ --		 Tablodaki Data Nasil Update Edilir (UPDATE SET)?
+ -- DML --> UPDATE
+ 
+ 
+ CREATE TABLE tedarikciler -- parent
+(
+vergi_no int PRIMARY KEY,
+firma_ismi VARCHAR(50),
+irtibat_ismi VARCHAR(50)
+);
+
+CREATE TABLE urunler -- child
+(
+ted_vergino int, 
+urun_id int, 
+urun_isim VARCHAR(50), 
+musteri_isim VARCHAR(50),
+CONSTRAINT fk_urunler FOREIGN KEY(ted_vergino) 
+REFERENCES tedarikciler(vergi_no)
+on delete cascade
+);
+
+INSERT INTO tedarikciler VALUES (101, 'IBM', 'Kim Yon');
+INSERT INTO tedarikciler VALUES (102, 'Huawei', 'Çin Li');
+INSERT INTO tedarikciler VALUES (103, 'Erikson', 'Maki Tammen');
+INSERT INTO tedarikciler VALUES (104, 'Apple', 'Adam Eve');
+
+INSERT INTO urunler VALUES(101, 1001,'Laptop', 'Ayşe Can');
+INSERT INTO urunler VALUES(102, 1002,'Phone', 'Fatma Aka');
+INSERT INTO urunler VALUES(102, 1003,'TV', 'Ramazan Öz');
+INSERT INTO urunler VALUES(102, 1004,'Laptop', 'Veli Han');
+INSERT INTO urunler VALUES(103, 1005,'Phone', 'Canan Ak');
+INSERT INTO urunler VALUES(104, 1006,'TV', 'Ali Bak');
+INSERT INTO urunler VALUES(104, 1007,'Phone', 'Aslan Yılmaz');
+
+DROP TABLE if exists urunler -- Eger tablo varsa tabloyu siler
+
+select * from tedarikciler;
+select * from urunler;
+
+-- vergi_no’su 102 olan tedarikcinin firma ismini 'Vestel' olarak güncelleyeniz.
+UPDATE tedarikciler SET firma_ismi = 'Vestel' WHERE vergi_no=102;
+
+-- vergi_no’su 101 olan tedarikçinin firma ismini 'casper' ve irtibat_ismi’ni 'Ali Veli' olarak güncelleyiniz.
+UPDATE tedarikciler SET firma_ismi = 'casper' WHERE vergi_no=101;
+UPDATE tedarikciler SET irtibat_ismi = 'Ali Veli' WHERE vergi_no=101;
+
+UPDATE tedarikciler SET firma_ismi = 'casper', irtibat_ismi='Ali Veli' WHERE vergi_no=101;
+
+
+-- urunler tablosundaki 'Phone' değerlerini 'Telefon' olarak güncelleyiniz.
+UPDATE urunler SET urun_isim = 'Telefon' WHERE urun_isim='Phone';
+
+-- urunler tablosundaki urun_id değeri 1004'ten büyük olanların urun_id’sini 1 arttırın.
+update urunler set urun_id = urun_id+1 where urun_id>1004;
+
+
+-- urunler tablosundaki tüm ürünlerin urun_id değerini ted_vergino sutun değerleri
+-- ile toplayarak güncelleyiniz
+update urunler set urun_id = urun_id + ted_vergino;
+
+/* urunler tablosundan Ali Bak’in aldigi urunun ismini, tedarikci tablosunda irtibat_ismi 
+'Adam Eve' olan firmanın ismi (firma_ismi) ile degistiriniz.
+-- Bu update işlemini yapmadan önce, tabloları eski haline getirmeliyiz.*/
+
+UPDATE urunler 
+SET urun_isim = (select firma_ismi from tedarikciler WHERE irtibat_ismi = 'Adam Eve')
+WHERE musteri_isim='Ali Bak';
+
+
+--Urunler tablosunda laptop satin alan musterilerin ismini, firma_ismi Apple’in 
+--irtibat_isim'i ile degistirin.
+
+UPDATE urunler
+SET musteri_isim = (select irtibat_ismi from tedarikciler WHERE firma_ismi = 'Apple')
+WHERE urun_isim = 'Laptop'
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
   
   
   
